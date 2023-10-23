efdeb7944d7468d20c8acf916e728ffe8a32dc0a
Svancik <thesvancik@gmail.com>
2023-10-23 23:30:05 +0200

    topbar

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.scss";
+import { Topbar } from "./components/topbar/Topbar";
+
+function App() {
+  return (
+    <div className="appWrapper">
+      <Topbar />
+    </div>
+  );
+}
+
+export default App;
