c76b21fa8ab95fb0b17980e5336901b0c317989d
Svancik <thesvancik@gmail.com>
2023-10-25 22:30:47 +0200

    about

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import logo from "./logo.svg";
 import "./App.scss";
 import { Topbar } from "./components/topbar/Topbar";
+import { About } from "./components/about/About";
 
 function App() {
   return (
     <div className="appWrapper">
       <Topbar />
+      <About />
     </div>
   );
 }
