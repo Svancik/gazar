6828335ddb37acc39f75b2dcb3f41a034ddea8ac
Svancik <thesvancik@gmail.com>
2023-12-03 00:56:51 +0100

    contactForm

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,7 +1,30 @@
 import React from "react";
 import "./hero.scss";
+import { useState } from "react";
+import MailIcon from "@mui/icons-material/Mail";
+import CheckIcon from "@mui/icons-material/Check";
+import Check from "@mui/icons-material/Check";
 
 export const Hero = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // You can handle form submission logic here, such as sending data to a server.
+    console.log("Form submitted:", formData);
+  };
+
   return (
     <div className="heroWrap">
       <video
@@ -9,8 +32,8 @@ export const Hero = () => {
         loop
         autoPlay
         muted
-        width="300px"
-        height="530px"
+        width="355px"
+        height="630px"
       >
         <source
           src={require(`../../media/vlaknoZarovky.mp4`)}
@@ -39,6 +62,80 @@ export const Hero = () => {
           quisquam excepturi natus, voluptate dignissimos praesentium fuga
           recusandae?
         </span>
+        <br /> <br />
+        <div className="heroAboutLinks">
+          <span>Dodáváme světelné zdroje do několika odvětví:</span>
+          <div className="heroTextCategories">
+            <div className="heroTextBox">Optika</div>
+            <div className="heroTextBox">Zdravotnictví</div>
+            <div className="heroTextBox">Průmysl</div>
+          </div>
+        </div>
+      </div>
+
+      <div className="heroContactForm">
+        <div className="contactFormText">
+          <MailIcon
+            className="mui"
+            sx={{
+              width: "28px",
+              height: "28px",
+            }}
+          />
+          <div className="contactUs">
+            <span>Kontaktujte nás</span>
+          </div>
+        </div>
+
+        <form onSubmit={handleSubmit}>
+          <div className="formRow">
+            <label>
+              Jméno
+              <input
+                type="text"
+                name="name"
+                value={formData.name}
+                onChange={handleChange}
+              />
+            </label>
+            <br />
+
+            <label>
+              Email
+              <input
+                type="email"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+              />
+            </label>
+          </div>
+          <br />
+
+          <label>
+            <textarea
+              rows="8"
+              cols="51"
+              name="message"
+              placeholder="Zadejte technické zadání vašeho požadavku. Jaký typ svítidla hledáte? Kolik kusů? Do jakého termínu?"
+              value={formData.message}
+              onChange={handleChange}
+            />
+          </label>
+          <br />
+
+          <button type="submit" className="heroFormSubmitBtn">
+            {" "}
+            <Check
+              className="mui"
+              sx={{
+                width: "24px",
+                height: "24px",
+              }}
+            />{" "}
+            <div className="submitBtnText">Odeslat</div>
+          </button>
+        </form>
       </div>
     </div>
   );
